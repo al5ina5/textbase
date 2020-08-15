c10b0b7c037afeaf8beb9a0aff255708628e6e77
Sebastian Alsina <alsinas@me.com>
2020-08-15 06:58:13 -0400

    Latest

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 #!/usr/bin/env node
 
+const fs = require('fs-extra')
 const { program } = require('commander')
 const lib = require('./lib/lib')
 
 program.parse(process.argv)
-
 console.log('')
-switch (program.args[0]) {
-    case 'eject':
-        lib.eject()
-        break
-    case 'build':
-        lib.build()
-        break
-    case 'start':
-        lib.start()
-        break
-    default:
-        lib.dev()
-        break
+
+var options = {
+    publicFolder: './public',
+    pagesFolder: './pages',
+    siteFolder: './_site',
+    templateFolder: __dirname + '/templates/textbase',
+    minify: false,
+    dev: false,
+    port: 0,
+    showExtensions: false,
 }
+
+var optionsFile = process.cwd() + '/_textbase.js'
+fs.exists(optionsFile, (exists) => {
+    if (exists) options = { ...options, ...require(optionsFile) }
+
+    global.options = options
+
+    switch (program.args[0]) {
+        case 'eject':
+            lib.eject()
+            break
+        case 'build':
+            lib.build()
+            break
+        case 'start':
+            lib.start()
+            break
+        default:
+            global.options = { ...global.options, ...{ dev: true } }
+            lib.dev()
+            break
+    }
+})
